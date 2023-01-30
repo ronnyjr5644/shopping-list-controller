f7555c7de4aee632f4ed1507eb2f1d7f27512ea6
ronnyjr5644 <raunakraj20092000@gmail.com>
2023-01-30 22:51:42 +0530

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
 (function () {
-'use strict';
-
-var shoppingList = [
-  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-];
-
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
-
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
-  $scope.shoppingList = shoppingList;
-}
-
-})();
+  'use strict';
+  
+  angular.module('ShoppingListApp', [])
+  .controller('ShoppingListAddController', ShoppingListAddController)
+  .controller('ShoppingListShowController', ShoppingListShowController)
+  .service('ShoppingListService', ShoppingListService);
+  
+  ShoppingListAddController.$inject = ['ShoppingListService'];
+  function ShoppingListAddController(ShoppingListService) {
+    var itemAdder = this;
+  
+    itemAdder.itemName = "";
+    itemAdder.itemQuantity = "";
+  
+    itemAdder.addItem = function () {
+      ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+    }
+  }
+  
+  
+  ShoppingListShowController.$inject = ['ShoppingListService'];
+  function ShoppingListShowController(ShoppingListService) {
+    var showList = this;
+  
+    showList.items = ShoppingListService.getItems();
+  
+    showList.removeItem = function (itemIndex) {
+      ShoppingListService.removeItem(itemIndex);
+    };
+  }
+  
+  
+  function ShoppingListService() {
+    var service = this;
+  
+    // List of shopping items
+    var items = [];
+  
+    service.addItem = function (itemName, quantity) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      items.push(item);
+    };
+  
+    service.removeItem = function (itemIndex) {
+      items.splice(itemIndex, 1);
+    };
+  
+    service.getItems = function () {
+      return items;
+    };
+  }
+  
+  })();
+  
